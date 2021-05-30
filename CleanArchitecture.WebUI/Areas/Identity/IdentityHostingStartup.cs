d58b5c81ef3ce974848e1db192a1b9bc3d4b38d5
KlaidDias <klaiddias@gmail.com>
2021-05-30 20:04:43 +0100

    Add custom scaffold Identity

---
+++ CleanArchitecture.WebUI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CleanArchitecture.Infra.Data.DbContext;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CleanArchitecture.WebUI.Areas.Identity.IdentityHostingStartup))]
+namespace CleanArchitecture.WebUI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
